1a0a38760af18793c5df4e46c91ec726efa22b74
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-04-05 09:59:01 +0400

    I completed but i not sleep

---
+++ src/script/app.js
@@ -0,0 +1,15 @@
+// let p = document.querySelector("#son");
+// let header = document.querySelector(".nav-section");
+
+// let headerObserver = new IntersectionObserver(
+//   (entries) => {
+//     const [watch] = entries;
+//     if (watch.isIntersecting) {
+//       header.classList.add("sticky-header");
+//     } else {
+//       header.classList.remove("sticky-header");
+//     }
+//   },
+//   { threshold: 1 }
+// );
+// headerObserver.observe(p);
